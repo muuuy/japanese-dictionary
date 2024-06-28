587b1d50f3d0a8f185bd478667e6475fd8a6a58f
Matthew Yu <myu2@umbc.edu>
2024-06-28 18:59:33 -0400

    learned ref (TS

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,36 @@
+import { useRef } from "react";
+
 import Logo from "../assets/yu_kana.png";
 
+import { HamburgerIcon } from "@chakra-ui/icons";
+
 const Navbar = () => {
+  const sidebar = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (sidebar.current)
+      sidebar.current.style.display =
+        sidebar.current.style.display === "flex" ? "none" : "flex";
+  };
+
   return (
-    <div className="flex flex-col h-screen w-fit bg-light-gray gap-4">
-      <img src={Logo} className="scale-50 h-16"  />
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
+    <div>
+      <HamburgerIcon
+        boxSize="32px"
+        className="ml-2 mt-4 cursor-pointer absolute"
+        onClick={handleClick}
+      />
+      <div
+        className="flex flex-col h-screen w-fit bg-light-gray gap-4"
+        ref={sidebar}
+      >
+        <img src={Logo} className="scale-50 h-16 select-none" />
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+      </div>
     </div>
   );
 };
