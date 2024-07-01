78ddfc62e588a2dfc1102f2b538c5b6a9956154a
Matthew Yu <myu2@umbc.edu>
2024-07-01 09:26:04 -0400

    fixed some stuff

--- client/src/pages/WhiteboardForm.tsx
+++ client/src/pages/WhiteboardForm.tsx
@@ -4,14 +4,13 @@ import CreateRoomForm from "../components/WhiteboardFormComponents/CreateRoomFor
 const WhiteboardForm = () => {
   return (
     <>
-      <div className="absolute inset-0 top-60 flex flex-col items-center">
+      <div className="inset-0 top-60  absolute flex flex-col items-center">
         <h1 className="text-5xl font-black">WHITEBOARD</h1>
         <h2 className="text-2xl font-semibold italic">
           Practice writing characters by yourself or with others!
         </h2>
       </div>
-
-      <div className="flex flex-row">
+      <div className="flex flex-row flex-1">
         <div className="flex flex-col w-1/2 h-screen justify-center items-center">
           <h3 className="text-4xl font-black">CREATE ROOM</h3>
           <p className="text-center mb-2 italic">
@@ -22,7 +21,9 @@ const WhiteboardForm = () => {
         </div>
         <div className="flex flex-col w-1/2 h-screen justify-center items-center">
           <h3 className="text-4xl font-black">JOIN ROOM</h3>
-          <p className="text-center mb-2 italic">Join an existing room to practice with others!</p>
+          <p className="text-center mb-2 italic">
+            Join an existing room to practice with others!
+          </p>
           <JoinRoomForm />
         </div>
       </div>
