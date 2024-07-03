f08ca365418940fd2920fb161c2ad0305bdf3cdf
Matthew Yu <myu2@umbc.edu>
2024-07-02 20:10:37 -0400

    worked on some backend (ejs, socket.io)

--- server/server.js
+++ server/server.js
@@ -1,15 +1,38 @@
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+require("dotenv").config();
 
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const server = createServer(app);
+const io = new Server(server);
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res, next) => {
-  res.send("testing");
+  res.render("index");
 });
 
 io.on("connection", (socket) => {
   console.log("User connected.");
+  socket.on("disconnect", () => {
+    console.log("User disconnected.");
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 });
 
 const port = process.env.PORT || 5000;
