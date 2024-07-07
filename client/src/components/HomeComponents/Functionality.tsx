72d5ad5869d0355e1a5158a24e0a985d586569db
Matthew Yu <myu2@umbc.edu>
2024-07-07 00:09:59 -0400

    style to functionality

--- client/src/components/HomeComponents/Functionality.tsx
+++ client/src/components/HomeComponents/Functionality.tsx
@@ -2,10 +2,10 @@ import HomePageImage from "../../assets/homepage_img.jpg";
 
 const Functionality = () => {
   return (
-    <section className="flex flex-row flex-1 border-t-2 border-black flex-1 h-2/5 gap-2 pb-2">
-      <img src={HomePageImage} className="rounded-3xl" />
-      <div className="flex flex-row justify-evenly bg-white rounded-3xl">
-        <div className="flex flex-col ">
+    <section className="flex flex-row flex-1 border-t-2 border-black flex-1 h-2/5">
+      <img src={HomePageImage} />
+      <div className="flex flex-row flex-1 justify-evenly bg-white">
+        <div className="flex flex-col w-1/3 justify-center items-center text-center p-2">
           <h3 className="text-3xl font-black">WHITEBOARD</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -17,11 +17,29 @@ const Functionality = () => {
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
-        <div>
-          <h3>FLASHCARDS</h3>
+        <div className="flex flex-col w-1/3 justify-center items-center text-center p-2">
+          <h3 className="text-3xl font-black">FLASHCARDS</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </div>
-        <div>
-          <h3>QUIZ</h3>
+        <div className="flex flex-col w-1/3 justify-center items-center text-center p-2">
+          <h3 className="text-3xl font-black">QUIZ</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </div>
       </div>
     </section>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,9 +7,9 @@ import { Button } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <div className="flex flex-1 flex-col max-h-screen max-w-full bg-black">
-      <section className="flex flex-row h-3/5 w-full justify-center items-center gap-2 py-2">
-        <div className="flex flex-col flex-1 justify-center items-center text-center bg-white h-full rounded-3xl">
+    <div className="flex flex-1 flex-col max-h-screen w-full bg-black overflow-hidden">
+      <section className="flex flex-row h-3/5 w-full justify-center items-center">
+        <div className="flex flex-col flex-1 justify-center items-center text-center bg-white h-full">
           <h1 className="font-black">
             <span className="text-4xl">STUDY AND LEARN</span>
             <br />
@@ -20,22 +20,19 @@ const Home = () => {
             with Friends
           </h2>
           <div className="flex flex-row gap-8 w-[400px]">
-            <Link to={"/signup"} className="w-[50%]">
+            <Link to={"/signup"} className="w-1/2">
               <Button colorScheme="red" variant={"outline"} width={"100%"}>
                 <span className="font-black">SIGN UP</span>
               </Button>
             </Link>
-            <Link to={"/login"} className="w-[50%]">
+            <Link to={"/login"} className="w-1/2">
               <Button colorScheme="red" width={"100%"}>
                 <span className="font-black">LOG IN</span>
               </Button>
             </Link>
           </div>
         </div>
-        <img
-          src={HomePageImage2}
-          className="max-w-[60%] h-[100%] object-cover rounded-3xl"
-        />
+        <img src={HomePageImage2} className="w-3/5 h-full object-fit" />
       </section>
       <Functionality />
     </div>
