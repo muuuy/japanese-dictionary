f8516e165f7ddaa29b348742284fa7138cbcbe4c
Matthew Yu <myu2@umbc.edu>
2024-07-11 14:22:07 -0400

    added the group chat

--- server/src/server.js
+++ server/src/server.js
@@ -53,6 +53,10 @@ io.on("connection", (socket) => {
   });
 });
 
+app.use("/", (req, res) => {
+  res.render("index");
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
   console.log(`Server is running on ${process.env.PORT || 5000}`)
