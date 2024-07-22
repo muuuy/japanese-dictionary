c74c806e431a454badb1b97c29a191659bdc28ce
Matthew Yu <myu2@umbc.edu>
2024-07-21 22:10:47 -0400

    worked on passport

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -2,4 +2,16 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("../models/User");
 
-passport.use("local-signup");
+passport.use(
+  "local-signup",
+  new LocalStrategy(async function verify(email, password, callback) {
+    try {
+      let user = await User.findOne({ email: email }).exec();
+
+      if (user)
+        return callback(null, false, { message: "Email already exists." });
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
