22f766fda43fc1d299cfb3c1ebd35d64fa89eca9
Matthew Yu <myu2@umbc.edu>
2024-07-23 00:41:16 -0400

    worked a bit on frontend

--- client/src/components/HomeComponents/Header.tsx
+++ client/src/components/HomeComponents/Header.tsx
@@ -1,9 +1,12 @@
 import HomePageImage2 from "../../assets/homepage_img2.jpg";
 import LoginBanner from "../LoginBanner";
-
+import useUserStore from "../../stores/store";
 import { SkeletonData } from "../../interfaces";
+import Typewriter from "../Typewriter";
 
 const Header: React.FC<SkeletonData> = ({ imageLoaded, imagesLoaded }) => {
+  const auth = useUserStore((state) => state.auth);
+
   return (
     <section className="flex flex-row h-3/5 w-full justify-center items-center">
       <div
@@ -20,8 +23,21 @@ const Header: React.FC<SkeletonData> = ({ imageLoaded, imagesLoaded }) => {
           Explore and enhance your Japanese language skills by yourself or with
           friends!
         </h2>
-        <div className="flex flex-row gap-8 w-[400px]">
-          <LoginBanner />
+        <div className="flex flex-row gap-8 w-[400px] justify-center relative">
+          {auth ? (
+            <div className="flex flex-col justify-center gap-4 absolute top-20">
+              <h3 className="text-4xl font-black italic underline">WELCOME BACK!</h3>
+              <div className="bg-red-500 rounded-full">
+                <Typewriter
+                  text="いらっしゃいませ！"
+                  speed={200}
+                  style="font-semibold italic text-white text-2xl"
+                />
+              </div>
+            </div>
+          ) : (
+            <LoginBanner />
+          )}
         </div>
       </div>
       <img
