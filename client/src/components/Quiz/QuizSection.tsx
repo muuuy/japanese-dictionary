9b71812810ee65b114f219663dc691d5c8053020
Matthew Yu <myu2@umbc.edu>
2024-07-24 13:51:06 -0400

    started working on button

--- client/src/components/Quiz/QuizSection.tsx
+++ client/src/components/Quiz/QuizSection.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import { Button } from "@chakra-ui/react";
+import { FaArrowRightToBracket } from "react-icons/fa6";
+
 interface QuizSectionData {
   active: string | null;
   setActive: (e: React.MouseEvent<HTMLDivElement>) => void;
@@ -22,9 +26,17 @@ const QuizSection: React.FC<QuizSectionData> = ({
       id={sectionID}
       onClick={(e) => setActive(e)}
     >
-      <h2 className="font-black text-5xl text-white italic text-center absolute bottom-0 bg-black p-4 w-full">
-        {sectionName}
-      </h2>
+      {active === sectionID ? (
+        <Link to={"/quiz/matching-quiz"} className="absolute bottom-20">
+          <Button>
+            <FaArrowRightToBracket />
+          </Button>
+        </Link>
+      ) : (
+        <h2 className="font-black text-5xl text-white italic text-center absolute bottom-0 bg-black p-4 w-full">
+          {sectionName}
+        </h2>
+      )}
     </div>
   );
 };
