a124d287fceb92fb74e1915c224b7f5f23f19844
Matthew Yu <myu2@umbc.edu>
2024-07-26 20:02:39 -0400

    added new login ui

--- client/src/components/LoginPrompt.tsx
+++ client/src/components/LoginPrompt.tsx
@@ -4,17 +4,19 @@ import LoginPromptImage from "../assets/login_prompt.jpg";
 const LoginPrompt = () => {
   return (
     <div className="flex flex-1  justify-center items-center">
-      <div className="flex flex-row max-h-96 max-w-4xl border-black border-2 overflow-hidden ">
-        <div className="flex flex-col w-1/2 justify-center items-center">
-          <h1 className="font-black text-5xl italic">LOGIN / SIGNUP</h1>
-          <h2 className="font-semibold text-xl">
-            Login or signup in order to access this content!
-          </h2>
-          <div className="flex flex-row gap-4 mt-4">
-            <Button colorScheme="red">
+      <div className="flex flex-row max-w-4xl border-black border-2 overflow-hidden ">
+        <div className="flex flex-col w-1/2 justify-center items-center p-2">
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="font-black text-5xl italic">LOGIN / SIGNUP</h1>
+            <h2 className="font-semibold text-xl">
+              Login or signup in order to access this content!
+            </h2>
+          </div>
+          <div className="flex flex-row gap-4 mt-4 w-full">
+            <Button colorScheme="red" width={"50%"}>
               <span className="font-black">LOGIN</span>
             </Button>
-            <Button colorScheme="red" variant={"outline"}>
+            <Button colorScheme="red" variant={"outline"} width={"50%"}>
               <span className="font-black">SIGN UP</span>
             </Button>
           </div>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,6 +5,7 @@ import useUserStore from "../stores/store";
 import { FormControl, FormLabel, Button, Input } from "@chakra-ui/react";
 import ErrorBanner from "../components/ErrorBanner";
 import { ErrorBannerData } from "../interfaces";
+import LoginPromptImage from "../assets/login_prompt.jpg";
 
 interface LoginData {
   email: string;
@@ -82,60 +83,65 @@ const Login = () => {
 
   return (
     <div className="flex flex-1 justify-center items-center text-center">
-      <div className="flex flex-col w-96">
-        <h1 className="user-form--header my-4">LOGIN</h1>
-        <form onSubmit={handleSubmit}>
-          <FormControl className="text-center" isRequired>
-            <FormLabel htmlFor="login--email">EMAIL</FormLabel>
-            <Input
-              id="login--email"
-              name="email"
-              type="email"
-              placeholder="Email"
-              autoComplete="email"
-              minLength={2}
-              maxLength={254}
-              onChange={handleInput}
-              className="mb-4"
-              value={formData.email}
-            />
-            <FormLabel htmlFor="login--password">PASSWORD</FormLabel>
-            <Input
-              id="login--password"
-              name="password"
-              type="password"
-              placeholder="Password"
-              minLength={8}
-              maxLength={32}
-              onChange={handleInput}
-              value={formData.password}
-            />
-            <p className="text-right">
-              <Link
-                to={"/forgot-password/"}
-                className="text-violet-500 font-black italic font-bold text-xs"
+      <div className="flex flex-row justify-center items-center border-2 border-black max-h-2xl max-w-5xl rounded-xl overflow-hidden  ">
+        <div className="flex flex-col w-1/2 p-4">
+          <h1 className="user-form--header mt-4 mb-1 text-4xl">LOGIN</h1>
+          <p className="mb-4 italic">Welcome back! Sign in to start studying!</p>
+          <form onSubmit={handleSubmit}>
+            <FormControl className="text-center" isRequired>
+              <FormLabel htmlFor="login--email">EMAIL</FormLabel>
+              <Input
+                id="login--email"
+                name="email"
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+                minLength={2}
+                maxLength={254}
+                onChange={handleInput}
+                className="mb-4"
+                value={formData.email}
+              />
+              <FormLabel htmlFor="login--password">PASSWORD</FormLabel>
+              <Input
+                id="login--password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                minLength={8}
+                maxLength={32}
+                onChange={handleInput}
+                value={formData.password}
+              />
+              <p className="text-right">
+                <Link
+                  to={"/forgot-password/"}
+                  className="text-violet-500 font-black italic font-bold text-xs"
+                >
+                  Forgot password?
+                </Link>
+              </p>
+              <Button
+                colorScheme="red"
+                className="mt-4 mb-2"
+                type="submit"
+                onClick={handleSubmit}
+                isLoading={loading}
               >
-                Forgot password?
-              </Link>
-            </p>
-            <Button
-              colorScheme="red"
-              className="mt-4 mb-2"
-              type="submit"
-              onClick={handleSubmit}
-              isLoading={loading}
-            >
-              <span className="font-black">SUBMIT</span>
-            </Button>
-            <p className="italic font-semibold">
-              Don't have an account?{" "}
-              <Link to={"/signup/"} className="text-violet-500 font-black">
-                SIGN UP
-              </Link>
-            </p>
-          </FormControl>
-        </form>
+                <span className="font-black">SUBMIT</span>
+              </Button>
+              <p className="italic font-semibold">
+                Don't have an account?{" "}
+                <Link to={"/signup/"} className="text-violet-500 font-black">
+                  SIGN UP
+                </Link>
+              </p>
+            </FormControl>
+          </form>
+        </div>
+        <img src={LoginPromptImage} className="object-cover w-1/2" />
       </div>
+
       <div className="absolute bottom-8 right-8 flex flex-col gap-2">
         {errorBanners.map((banner, index) => (
           <ErrorBanner
