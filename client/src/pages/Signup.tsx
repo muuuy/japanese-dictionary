3888c05b364d6beb9629e419636551269918d075
Matthew Yu <myu2@umbc.edu>
2024-07-28 14:36:12 -0400

    added image load + sign up styling

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import { Skeleton } from "@chakra-ui/react";
 import { FormControl, FormLabel, Button, Input } from "@chakra-ui/react";
+import SignupImage from "../assets/signup_image.jpg";
 
 interface SignupData {
   email: string;
@@ -16,6 +17,7 @@ const Signup = () => {
     password: "",
     verifyPassword: "",
   });
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
@@ -35,64 +37,79 @@ const Signup = () => {
     }
   };
 
+  const handleImageLoaded = () => {
+    setImageLoaded(true);
+  };
+
   return (
-    <div className="flex flex-1 justify-center items-center text-center">
-      <div className="flex flex-col w-96">
-        <h1 className="user-form--header my-4">SIGNUP</h1>
-        <FormControl
-          className="flex-col justify-center items-center"
-          isRequired
-        >
-          <FormLabel htmlFor="signup--email">EMAIL</FormLabel>
-          <Input
-            id="signup--email"
-            name="email"
-            type="email"
-            placeholder="Email"
-            autoComplete="email"
-            minLength={2}
-            maxLength={254}
-            onChange={handleInput}
-            className="mb-4"
-            value={formData.email}
-          />
-          <FormLabel htmlFor="signup--password">PASSWORD</FormLabel>
-          <Input
-            id="signup--password"
-            name="password"
-            type="password"
-            placeholder="Password"
-            minLength={8}
-            maxLength={32}
-            onChange={handleInput}
-            className="mb-4"
-            value={formData.password}
-          />
-          <FormLabel htmlFor="signup--verify-password">
-            VERIFY PASSWORD
-          </FormLabel>
-          <Input
-            id="signup--verify-password"
-            name="verifyPassword"
-            type="password"
-            minLength={8}
-            maxLength={32}
-            placeholder="Password"
-            onChange={handleInput}
-            value={formData.verifyPassword}
-          />
-          <p className="italic font-semibold my-4">
-            Already have an account?{" "}
-            <Link to={"/login/"} className="text-violet-500 font-black">
-              LOGIN
-            </Link>
-          </p>
-          <Button colorScheme="red" onClick={handleSubmit}>
-            <span className="font-black">SUBMIT</span>
-          </Button>
-        </FormControl>
+    <Skeleton
+      className="flex flex-1 justify-center items-center text-center"
+      fadeDuration={1}
+      isLoaded={imageLoaded}
+    >
+      <div className="flex flex-row justify-center items-center border-2 border-black max-h-2xl max-w-5xl rounded-xl overflow-hidden shadow-2xl">
+        <div className="flex flex-col w-1/2 px-12">
+          <h1 className="user-form--header my-4">SIGNUP</h1>
+          <FormControl
+            className="flex-col justify-center items-center"
+            isRequired
+          >
+            <FormLabel htmlFor="signup--email">EMAIL</FormLabel>
+            <Input
+              id="signup--email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              autoComplete="email"
+              minLength={2}
+              maxLength={254}
+              onChange={handleInput}
+              className="mb-4"
+              value={formData.email}
+            />
+            <FormLabel htmlFor="signup--password">PASSWORD</FormLabel>
+            <Input
+              id="signup--password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              minLength={8}
+              maxLength={32}
+              onChange={handleInput}
+              className="mb-4"
+              value={formData.password}
+            />
+            <FormLabel htmlFor="signup--verify-password">
+              VERIFY PASSWORD
+            </FormLabel>
+            <Input
+              id="signup--verify-password"
+              name="verifyPassword"
+              type="password"
+              minLength={8}
+              maxLength={32}
+              placeholder="Password"
+              onChange={handleInput}
+              value={formData.verifyPassword}
+            />
+            <p className="italic font-semibold my-4">
+              Already have an account?{" "}
+              <Link to={"/login/"} className="text-violet-500 font-black">
+                LOGIN
+              </Link>
+            </p>
+            <Button colorScheme="red" onClick={handleSubmit}>
+              <span className="font-black">SUBMIT</span>
+            </Button>
+          </FormControl>
+        </div>
+        <img
+          src={SignupImage}
+          className="object-cover w-1/2"
+          onLoad={handleImageLoaded}
+        />
       </div>
-    </div>
+    </Skeleton>
   );
 };
 
