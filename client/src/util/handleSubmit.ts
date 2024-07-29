a73e6d59ae357bf707d49f2e427614d057d6bc1d
Matthew Yu <myu2@umbc.edu>
2024-07-28 21:24:39 -0400

    working on submit

---
+++ client/src/util/handleSubmit.ts
@@ -0,0 +1,44 @@
+import { LoginFormData } from "../interfaces";
+import { FlashcardData } from "../interfaces";
+
+export const handleSubmit = async (
+  loading: boolean,
+  setLoading: (value: boolean) => void,
+  formData: LoginFormData,
+  authUser: (flashcards: FlashcardData[]) => void,
+  navigate: (path: string) => void,
+  addErrorBanner: (title: string, message: string) => void
+) => {
+  if (loading) {
+    return;
+  }
+
+  setLoading(true);
+
+  try {
+    const res = await fetch("http://localhost:3000/users/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      authUser(data.flashcards);
+      navigate("/");
+    } else {
+      setLoading(false);
+
+      const response = await res.json();
+
+      console.log(response);
+      addErrorBanner("Error logging in!", response.errors[0].msg);
+    }
+  } catch (err) {
+    console.log(err, "test");
+    setLoading(false);
+  }
+};
