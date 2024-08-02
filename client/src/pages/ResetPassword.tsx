ff3980973435652cddadd5ee84b4845278cfdda5
Matthew Yu <myu2@umbc.edu>
2024-08-01 21:01:24 -0400

    started working on reset password

--- client/src/pages/ResetPassword.tsx
+++ client/src/pages/ResetPassword.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { FormControl, FormLabel, Button, Input } from "@chakra-ui/react";
 import ResetPasswordImage from "../assets/reset_password.jpg";
+import UserFormButton from "../components/UserFormComponents/UserFormButton";
 
 interface ResetData {
   password: string;
@@ -11,20 +12,21 @@ const ResetPassword = () => {
   const [formData, setFormData] = useState<ResetData>({
     password: "",
   });
+  const [loading, setLoading] = useState<boolean>(false);
   const token = useParams().token;
-
-  console.log(token);
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
+  const handleSubmit = () => {};
+
   return (
     <div className="page--container">
       <div className="user-form--container">
         <div className="user-form--form-container">
-          <h1 className="user-form--header my-4">FORGOT PASSWORD</h1>
+          <h1 className="user-form--header my-4">RESET PASSWORD</h1>
           <FormControl className="text-center" isRequired>
             <FormLabel htmlFor="reset--password">NEW PASSWORD</FormLabel>
             <Input
@@ -34,7 +36,7 @@ const ResetPassword = () => {
               placeholder="Password"
               onChange={handleInput}
             />
-            <FormLabel htmlFor="reset--verify-password">
+            <FormLabel htmlFor="reset--verify-password" className="mt-4">
               VERIFY NEW PASSWORD
             </FormLabel>
             <Input
@@ -44,9 +46,7 @@ const ResetPassword = () => {
               placeholder="Password"
               onChange={handleInput}
             />
-            <Button colorScheme="red" className="mt-4 mb-2">
-              <span className="font-black">SUBMIT</span>
-            </Button>
+            <UserFormButton handleSubmit={handleSubmit} loading={loading} />
             <p className="italic font-semibold">
               Return to{" "}
               <Link to={"/login/"} className="text-violet-500 font-black">
